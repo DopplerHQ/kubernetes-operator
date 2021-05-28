d3c83685c201b396fe9eb0ab58f16a619941cc2a
Nic Manoogian <nic.manoogian@doppler.com>
2021-05-28 13:23:20 -0400

    Add required RBAC markers

--- controllers/dopplersecret_controller.go
+++ controllers/dopplersecret_controller.go
@@ -38,6 +38,9 @@ type DopplerSecretReconciler struct {
 //+kubebuilder:rbac:groups=secrets.doppler.com,resources=dopplersecrets/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=secrets.doppler.com,resources=dopplersecrets/finalizers,verbs=update
 
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;delete
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=list;watch;get;update
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
